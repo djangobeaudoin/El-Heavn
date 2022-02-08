290e1f4e17cb6a9a9137c806237306e179ebbc50
Django Beaudoin <73009723+duhjangle25@users.noreply.github.com>
2022-02-08 10:59:29 -0700

    Basic AgentScript Movement
    Incorporate basic upwards movement of 'particles.'
    Use patches instead of turtles to represent trees.

--- basic-agentscript-model/model.js
+++ basic-agentscript-model/model.js
@@ -1,13 +1,22 @@
 import * as util from './src/utils.js'  // This will come in handy later
-import Model from './src/Model.js'
+import Model3D from './src/Model3D.js'
 
-export default class Wander3DModel extends Model {
+export default class CloudModel extends Model3D {
     trees = 100
 
     setup() {
         // Create "trees". These might work better as just... patches, something for later implementation. As it stands these exist just to prove that I can do... things.
         this.turtles.create(this.trees, t => {
             t.setxyz(...this.world.randomPoint(), -16)
+        })
+        // this.patches.setDefault('value', 0)
+        this.patches.ask(p => {
+            p.value = Math.random()
+        })
+    }
+    step() {
+        this.turtles.ask(t => {
+            t.setxyz(t.x, t.y, t.z + .05)
         })
     }
 }
