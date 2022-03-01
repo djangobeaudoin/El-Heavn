c8e90dd015229bd57d3217b895303514cecff3a8
Django Beaudoin <73009723+duhjangle25@users.noreply.github.com>
2022-03-01 12:57:29 -0700

    basic plume implementation

--- basic-agentscript-model/model.js
+++ basic-agentscript-model/model.js
@@ -4,27 +4,32 @@ import World from './src/World.js'
 
 export default class CloudModel extends Model3D {
     // Set world size
-    constructor(worldOptions = World.defaultOptions(100)) {
+    constructor(worldOptions = World.defaultOptions(64)) {
         super(worldOptions)
     }
 
     // Misc. Variables
-    co2Count = 100
-    h2oCount = 100
     treePercentage = 0.4
-    particleSpeed = 1
+    particleSpeed = 4
+
 
     setup() {
         // Setup Turtle Breeds and Types
         this.turtleBreeds('co2s h2os')
-        this.co2Type = 'co2'
+        // this.co2Type = 'co2'
         this.h2oType = 'h2o'
+        this.h2os.setDefault('atEdge', 'clamp')
+        // this.co2s.setDefault('atEdge', 'clamp')
+
+        // Water will be vapor by default
+        this.h2os.setDefault('liquid', true)
 
         // Setup Patch Breeds and Types
         this.patchBreeds('trees dirts fires')
-        this.treeType = 'tree'
         this.dirtType = 'dirt'
         this.fireType = 'fire'
+        this.treeType = 'tree'
+        this.fires.setDefault('ticks', 0)
         
         this.createPatches()
         this.patches.ask(p => {
@@ -37,27 +42,18 @@ export default class CloudModel extends Model3D {
         // Set tree to fire breed, which does nothing right now but someday it will
         treeToIgnite.setBreed(this.fires)
         treeToIgnite.type = this.fireType
-
-        // Create H2O
-        this.h2os.createOne(t => {
-            t.setxyz(treeToIgnite.x, treeToIgnite.y, this.world.minZ)
-            t.type = this.h2oType
-        })
-
-        // Create CO2
-        this.co2s.createOne(t => {
-            t.setxyz(treeToIgnite.x, treeToIgnite.y, this.world.minZ)
-            t.type = this.co2Type
-        })
+        treeToIgnite.ticks = this.ticks
     }
 
     createPatches() {
+        // Create Trees based on treePercentage
         this.patches.ask(p => {
             p.value = Math.random()
             if (Math.random() <= this.treePercentage) p.setBreed(this.trees)
             else p.setBreed(this.dirts)
         })
 
+        // Set Types
         this.trees.ask(p => {
             p.type = this.treeType
         })
@@ -70,7 +66,12 @@ export default class CloudModel extends Model3D {
     step() {
         // Currently moving all turtles up at an arbitrary rate.
         this.turtles.ask(t => {
-            t.setxyz(t.x, t.y, t.z + this.particleSpeed)
+            t.setxyz(t.x + (Math.random() -0.5)*2, t.y + (Math.random() -0.5)*2, t.z + this.particleSpeed)
+        })
+
+        // If the water is not liquid we don't care about it
+        this.h2os.ask(t => {
+            if (!t.liquid) t.die()
         })
 
         // Ignite neighboring trees
@@ -79,6 +80,17 @@ export default class CloudModel extends Model3D {
             p.neighbors.forLoop(agent => {
                 if (agent.type == this.treeType) this.igniteTree(agent)
             })
+
+            if (this.ticks > p.ticks + 30) {
+                p.setBreed(this.dirts)
+                p.type = this.dirtType
+            }
+
+            // Create H2O
+            this.h2os.createOne(t => {
+            t.setxyz(p.x, p.y, this.world.minZ)
+            t.type = this.h2oType
+        })
         })
     }
 }
