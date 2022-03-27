91cde4fcd8b5991087db40d0e201682db10d434d
Django Beaudoin <73009723+duhjangle25@users.noreply.github.com>
2022-03-27 11:34:47 -0600

    Notes

--- basic-agentscript-model/model.js
+++ basic-agentscript-model/model.js
@@ -2,6 +2,41 @@
 import Model3D from './src/Model3D.js'
 import World from './src/World.js'
 
+
+/**
+ * A few Notes:
+ * Assume each patch is 100m^2. This puts the total z range of the model at 12.8km, approximately the height of the troposphere
+ * the base of the forest is at 2.4km
+ * 
+ * Some other notes:
+ * water begins in vapor form, rises until it condenses
+ * Water molecules are attracted to each other, meaning clouds are congregations of liquid water
+ * 
+ * as altitude increases:
+ * temp decreases by 1C per 100m
+ * pressure decreases
+ * cool air and low pressure cause condensation, adiabatic process
+ * adiabatic meaning a gradual/smooth transition
+ * 
+ * assume chemical potential of liquid water = chem pot of water vapor
+ * 
+ * Key part of kelvin equation is relation between vapor pressure and droplet radius
+ * 
+ * implement things that aren't water for clouds to form on; ash/soot (CCNs)
+ * 
+ * Assumption stuff
+ * 0.43-1.82% of mass of burnt wood results in ash; assume 1% for simple
+ * 1 tree (C6H12O6) = 5.99co2 + 6h2o + 0.01CCN
+ * multiply everything by 100 for reasons?
+ * assume CCNs are carbon
+ * everything is variable
+ * 
+ * So to do:
+ * find ratio of CCN and h2o per tree
+ * temperature and pressure
+ * model condensation of water onto CCN
+ */
+
 export default class CloudModel extends Model3D {
     // Set world size
     constructor(worldOptions = World.defaultOptions(64)) {
@@ -15,12 +50,13 @@ export default class CloudModel extends Model3D {
 
     setup() {
         // Setup Turtle Breeds and Types
-        this.turtleBreeds('h2os')
+        this.turtleBreeds('h2os CCNs')
         this.h2oType = 'h2o'
+        this.CCNType = 'CCN'
 
         // Turtle Variables
-        this.h2os.setDefault('atEdge', 'clamp')
-        this.h2os.setDefault('ticks', 0)        // This ticks variable allows keeping track of how long the turtle has been alive
+        this.turtles.setDefault('atEdge', 'clamp')
+        this.turtles.setDefault('ticks', 0)        // This ticks variable allows keeping track of how long the turtle has been alive
         this.h2os.setDefault('liquid', true)
 
         // Setup Patch Breeds and Types
@@ -72,7 +108,7 @@ export default class CloudModel extends Model3D {
 
         // If the water is not liquid, don't worry about it
         this.h2os.ask(t => {
-            if (this.ticks > t.ticks + 50) t.liquid = false
+            if (this.ticks > t.ticks + 30) t.liquid = false
 
             if (!t.liquid) t.die()
         })
@@ -90,8 +126,8 @@ export default class CloudModel extends Model3D {
             }
 
             // Create H2O
-            // Currently only creating every other tick for the sake of not destroying computer
-            if (this.ticks % 2 === 0){
+            // Currently only creating every third tick for the sake of not destroying computer
+            if (this.ticks % 3 === 0){
                 this.h2os.createOne(t => {
                     t.setxyz(p.x, p.y, this.world.minZ)
                     t.type = this.h2oType
