1d122b23a437c3889392e329fb59c214d8ee83f6
Django Beaudoin <73009723+duhjangle25@users.noreply.github.com>
2022-04-02 15:12:19 -0600

    CCNs, Smoke Speed, Temperature
    Adds CCNs and refinements to the time scale of the model. Additionally adds temperature based on simple assumptions and smoke speed from real world measurements

--- basic-agentscript-model/model.js
+++ basic-agentscript-model/model.js
@@ -9,32 +9,67 @@ import World from './src/World.js'
  * the base of the forest is at 2.4km
  * 
  * Some other notes:
- * water begins in vapor form, rises until it condenses
- * Water molecules are attracted to each other, meaning clouds are congregations of liquid water
  * 
  * as altitude increases:
- * temp decreases by 1C per 100m
- * pressure decreases
- * cool air and low pressure cause condensation, adiabatic process
- * adiabatic meaning a gradual/smooth transition
- * 
- * assume chemical potential of liquid water = chem pot of water vapor
- * 
- * Key part of kelvin equation is relation between vapor pressure and droplet radius
- * 
- * implement things that aren't water for clouds to form on; ash/soot (CCNs)
- * 
- * Assumption stuff
- * 0.43-1.82% of mass of burnt wood results in ash; assume 1% for simple
- * 1 tree (C6H12O6) = 5.99co2 + 6h2o + 0.01CCN
- * multiply everything by 100 for reasons?
- * assume CCNs are carbon
- * everything is variable
+ * temp decreases by 1C per 100m (?)
  * 
  * So to do:
  * find ratio of CCN and h2o per tree
  * temperature and pressure
  * model condensation of water onto CCN
+ * 
+ * patches are 100m^2
+ * 
+ * ignition delay 150s/m,
+ * therefore each tick is 15000s, the ignition delay per patch.
+ * trees take 43 hours to burn completely; 157650s
+ * 1 tree = 15.765kg
+ * 1cm^3 = 162000CCN
+ * trees produce 1 litre of smoke / second
+ * entire model is 2,097.152km^3; 209,715,200cm^3
+ * moisture content of tree = 50%
+ * 
+ * 1 litre = 1000cm^3
+ * 162000000CCN per litre
+ * 2430000000000CCN per tick
+ * relative humidity = 44%
+ * 7.8825kg of water per tree (?)
+ * average temperature is 11.66667C
+ * average dew point is -1.666667
+ * 
+ * smoke moves ~60cm/s
+ * 6m/10s
+ * 60m/100s
+ * 
+ * smoke moves 9000m/tick?
+ * 
+ * so:
+ * each CCN sphere = 2430000000000CCN
+ * each H2O sphere = 0.716590909090909 litres
+ * trees burn for 11 ticks
+ * 
+ * if ticks = 15000s:
+ * trees burn 11 ticks
+ * smoke moves 9000m/tick
+ * 
+ * if ticks = 5000s
+ * trees burn 33 ticks
+ * smoke moves 3000m/tik
+ * 
+ * if ticks = 1000s
+ * trees burn 165 ticks
+ * smoke moves 600m/tick (reasonable-ish)
+ * 
+ * New Things:
+ * each CCN sphere = 2430000000000CCN
+ * each H2O sphere = 0.716590909090909 litres
+ * average temperature is 11.66667C
+ * average dew point is -1.666667
+ * Each tick = 1000s
+ * Trees burn 165 (?) ticks
+ * Smoke moves 600m/tick
+ * Smoke temp decreases 6C/tick
+ * For now, assume smoke begins at 100C; as soon as water vapor is created.
  */
 
 export default class CloudModel extends Model3D {
@@ -45,7 +80,7 @@ export default class CloudModel extends Model3D {
 
     // Misc. Variables
     treePercentage = 0.4
-    particleSpeed = 2
+    smokeSpeed = 6 // in 100m/s
 
 
     setup() {
@@ -55,9 +90,12 @@ export default class CloudModel extends Model3D {
         this.CCNType = 'CCN'
 
         // Turtle Variables
-        this.turtles.setDefault('atEdge', 'clamp')
+        this.h2os.setDefault('atEdge', 'clamp')
+        this.CCNs.setDefault('atEdge', 'clamp')
         this.turtles.setDefault('ticks', 0)        // This ticks variable allows keeping track of how long the turtle has been alive
-        this.h2os.setDefault('liquid', true)
+        this.h2os.setDefault('temp', 100)
+        this.CCNs.setDefault('temp', 100)
+        this.h2os.setDefault('liquid', false)
 
         // Setup Patch Breeds and Types
         this.patchBreeds('trees dirts fires')
@@ -77,7 +115,7 @@ export default class CloudModel extends Model3D {
     }
 
     igniteTree(treeToIgnite) {
-        // Set tree to fire breed, which does nothing right now but someday it will
+        // Set tree to fire breed
         treeToIgnite.setBreed(this.fires)
         treeToIgnite.type = this.fireType
         treeToIgnite.ticks = this.ticks
@@ -102,17 +140,22 @@ export default class CloudModel extends Model3D {
     step() {
         // Move Turtles
         this.turtles.ask(t => {
-            // Turtles move in brownian motion, constantly moving up
-            t.setxyz(t.x + (Math.random() -0.5)*2, t.y + (Math.random() -0.5)*2, t.z + this.particleSpeed)
+            /**
+             * Does this make sense?
+             * IE, yes smoke probably has some variance which this does model,
+             * but the scale is non-existent. Ie, every 15000s
+             */
+            if (t.temp > -1) {
+                t.setxyz(t.x + (Math.random() -0.5)*2, t.y + (Math.random() -0.5)*2, t.z + this.smokeSpeed)
+                t.temp -= 6
+            }
         })
 
-        // If the water is not liquid, don't worry about it
-        this.h2os.ask(t => {
-            if (this.ticks > t.ticks + 30) t.liquid = false
-
-            if (!t.liquid) t.die()
+        // Kill stuff after 30 ticks to not murder computer for prototyping
+        this.turtles.ask( t=> {
+            if (this.ticks > t.ticks + 30) t.die()
         })
-
+        
         // Ignite neighboring trees
         this.fires.ask(p => {
             // p.neighbors returns an AgentArray, which can be looped through with the forLoop function. This is more efficient and easier than something like for (var i = 0; length, i++), but achieves the same thing
@@ -120,20 +163,24 @@ export default class CloudModel extends Model3D {
                 if (agent.type == this.treeType) this.igniteTree(agent)
             })
 
-            if (this.ticks > p.ticks + 30) {
+            if (this.ticks > p.ticks + 11) {
                 p.setBreed(this.dirts)
                 p.type = this.dirtType
             }
 
             // Create H2O
-            // Currently only creating every third tick for the sake of not destroying computer
-            if (this.ticks % 3 === 0){
-                this.h2os.createOne(t => {
-                    t.setxyz(p.x, p.y, this.world.minZ)
-                    t.type = this.h2oType
-                    t.ticks = this.ticks
-                })
-            }
+            this.h2os.createOne(t => {
+                t.setxyz(p.x, p.y, this.world.minZ)
+                t.type = this.h2oType
+                t.ticks = this.ticks
+            })
+
+            // Create CCN
+            this.CCNs.createOne(t => {
+                t.setxyz(p.x, p.y, this.world.minZ)
+                t.type = this.CCNType
+                t.ticks = this.ticks
+            })
         })
     }
 }
